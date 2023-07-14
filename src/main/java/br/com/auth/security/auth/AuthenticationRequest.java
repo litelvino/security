572570584383e4af2ce35b9f0f20dc24fcc3783d
lucasitelvino <litelvin.57843@gmail.com>
2023-07-14 00:05:02 -0300

    Initial commit

---
+++ src/main/java/br/com/auth/security/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package br.com.auth.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
---
+++ src/main/java/br/com/auth/security/auth/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package br.com.auth.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+}
